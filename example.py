fe8ad0bebdfe6a2d22669b257a864fbd43d778f0
Mia Urosevic <miaurosevic21@gmail.com>
2026-02-04 12:05:52 -0800

    update example.py

--- example.py
+++ example.py
@@ -10,6 +10,10 @@ def is_even(n):
     HINT:
     Use the modulus operator %
 
+    #Any line that starts with >>> is called a "doctest"
+    #>>> is the "prompt" for python
+    #$ is the "prompt" for shell/terminal
+
     >>> is_even(0)
     True
     >>> is_even(1)
@@ -23,7 +27,7 @@ def is_even(n):
     >>> type(is_even(0))
     <class 'bool'>
     '''
-
+    return n%2==0
 
 def factorial(n):
     '''
@@ -47,7 +51,13 @@ def factorial(n):
     >>> factorial(100)
     93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
     '''
-
+    # define a varibk just before a for loop
+    # modify that variabl in each iteration
+    accumulator=1
+    for i in range(1,n+1):
+        accumulator *= i
+    return accumulator
+"""
 
 def largest(xs):
     '''
@@ -84,3 +94,4 @@ def filter_odd(xs):
     >>> filter_odd([20,13,4,16,8,19,10])
     [20, 4, 16, 8, 10]
     '''
+"""   
